0933a5ed5000b7382812f230c05902cc487a5844
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-30 12:23:36 -0300

    Correção de funções, da classe e do vetor.

--- CadastrarLivros/Livro.cs
+++ CadastrarLivros/Livro.cs
@@ -1,138 +1,39 @@
-﻿public class Livro
+﻿internal class Livro
 {
-    int quantidade;
-    Livro[] estante = new Livro[10];
-    public string Titulo;
-    public string Autor;
-    public DateOnly Lancamento;
-    public string Editora;
-    public string Edicao;
-    public int ISBN;
-    public int Paginas;
+    //DEFININDO ATRIBUTOS
+    string Titulo;
+    string Autor;
+    DateOnly Lancamento;
+    string Editora;
+    int Edicao;
+    string ISBN;
+    int Paginas;
 
-    public Livro()
+    //FAZENDO O CONSTRUTOR
+    public Livro(string titulo, string autor, DateOnly lancamento, string editora, int edicao, string iSBN, int paginas)
     {
+        Titulo = titulo;
+        Autor = autor;
+        Lancamento = lancamento;
+        Editora = editora;
+        Edicao = edicao;
+        ISBN = iSBN;
+        Paginas = paginas;
+    }
+
+    //MÉTODO PARA IMPRIMIR AS INFORMAÇÕES DO LIVRO
+    public void imprimirLivro()
+    {
+        Console.WriteLine("\n\n---- INFORMAÇÕES DO LIVRO ----");
+        Console.WriteLine("Título: " + this.Titulo);
+        Console.WriteLine("Autor: " + this.Autor);
+        Console.WriteLine("Data de Lançamento: " + this.Lancamento);
+        Console.WriteLine("Editor: " + this.Editora);
+        Console.WriteLine("Edição: " + this.Edicao);
+        Console.WriteLine("ISBN: " + this.ISBN);
+        Console.WriteLine("Quantidade de Páginas: " + this.Paginas);
 
     }
 
-    public void definirTitulo(string titulo)
-    {
-        this.Titulo = titulo;
-    }
 
-    public void definirAutor(string autor)
-    {
-        this.Autor = autor;
-    }
-
-    public void definirLancamento(DateOnly data)
-    {
-        this.Lancamento = data;
-    }
-
-    public void definirEditora(string editora)
-    {
-        this.Editora = editora;
-    }
-
-    public void definirEdicao(string edicao)
-    {
-        this.Edicao = edicao;
-    }
-
-    public void definirISB(int isbn)
-    {
-        this.ISBN = isbn;
-    }
-
-    public void definirPaginas(int paginas)
-    {
-        this.Paginas = paginas;
-    }
-
-    public Livro criarLivro() //Método para criar livros com seus atributos.
-    {
-        Livro livro = new Livro();
-        Console.WriteLine("--------------------------------------------------------");
-        Console.WriteLine("\nInsira o titulo do livro: ");
-        livro.definirTitulo(Console.ReadLine());
-
-        Console.WriteLine("Insira o autor do livro: ");
-        livro.definirAutor(Console.ReadLine());
-
-        Console.WriteLine("Insira a data de lançamento do livro: ");
-        livro.definirLancamento(DateOnly.Parse(Console.ReadLine()));
-
-        Console.WriteLine("Insira a editora do livro: ");
-        livro.definirEditora(Console.ReadLine());
-
-        Console.WriteLine("Insira a edição do livro: ");
-        livro.definirEdicao(Console.ReadLine());
-
-        Console.WriteLine("Insira o número do ISBN do livro: ");
-        livro.definirISB(int.Parse(Console.ReadLine()));
-
-        Console.WriteLine("Insira a quantidade de páginas do livro: ");
-        livro.definirPaginas(int.Parse(Console.ReadLine()));
-
-        return livro;
-    }
-
-
-    public void inserirLivros() //Repete o looping de criar livros, de acordo com a quantidade de livros
-    {
-        do
-        {
-            Console.Write("\n\n|Digite quantos livros deseja inserir: ");
-            quantidade = int.Parse(Console.ReadLine());
-            if (quantidade < 0 || quantidade > 10)
-            {
-                Console.WriteLine("\nValor inválido! \n| Mínimo: 1 livro | Máximo: 10 livros|\n");
-            }
-        } while (quantidade < 0 || quantidade > 10);
-
-        for (int i = 0; i < quantidade; i++)
-        {
-            estante[i] = criarLivro(); //Usa função criar livros 
-        }
-    }
-
-    public void imprimirInfo() //Estrutura de impressão das informações dos livros
-    {
-        Console.WriteLine("\n----- INFORMAÇÕES DO LIVRO ----\n");
-        Console.WriteLine("Título: {0}", this.Titulo);
-        Console.WriteLine("Autor(es): {0}", this.Autor);
-        Console.WriteLine("Data de Lançamento: {0}", this.Lancamento);
-        Console.WriteLine("Editora: {0}", this.Editora);
-        Console.WriteLine("Edição: {0}", this.Edicao);
-        Console.WriteLine("ISBN: {0}", this.ISBN);
-        Console.WriteLine("Quantidade de Páginas: {0}", this.Paginas);
-    }
-
-    public void ImprimirLivros() //Imprime todos os livros 
-    {
-        for (int i = 0; i < estante.Length; i++)
-        {
-            if (estante[i] != null)
-            {
-                Console.WriteLine($"\n\n|Livro na posição {i + 1}|:");
-                estante[i].imprimirInfo(); //Usa o método imprimirInfo
-            }
-        }
-    }
-
-    public void ImprimirPosicao()
-    {
-        Console.WriteLine("\nDigite o número da posição de um livro: ");
-        int posicao = int.Parse(Console.ReadLine());
-        if (posicao >= 1 && posicao <= quantidade)
-        {
-            Console.WriteLine($"Livro na posição {posicao}:");
-            estante[posicao - 1].imprimirInfo();
-        }
-        else
-        {
-            Console.WriteLine("Posição inválida.");
-        }
-    }
 }
--- CadastrarLivros/Program.cs
+++ CadastrarLivros/Program.cs
@@ -1,44 +1,64 @@
-﻿Livro livro = new Livro();
-while (true)
+﻿//VARIAVEIS GLOBAIS
+int indice = 0;
+int op = 0;
+
+
+//CRIANDO O VETOR ESTANTE QUE UTILIZA A CLASSE LIVRO
+Livro[] estante = new Livro[10]; 
+
+
+//MÉTODO PARA CRIAR LIVROS
+Livro cadastrarLivro()
 {
-    void Menu()
-    {
-        bool TrueFalse = false;
-        int opcao = 0;
-        do
-        {
+    //VARIAVEIS LOCAIS
+    string titulo, autor, editora, isbn;
+    int edicao, paginas;
+    DateOnly lancamento;
 
-            Console.WriteLine("\n---- MENU ----");
-            Console.WriteLine("\n|1| - Adicionar um Livro");
-            Console.WriteLine("|2| - Imprimir a Estante");
-            Console.WriteLine("|3| - Imprimir um Livro");
-            Console.Write("\nEscolha uma opção: ");
-            opcao = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o título do livro: ");
+    titulo = Console.ReadLine();
 
-        } while (TrueFalse == true);
+    Console.WriteLine("Informe o autor do livro: ");
+    autor = Console.ReadLine();
 
-        switch (opcao)
-        {
-            case 1:
-                livro.inserirLivros();
-                break;
+    Console.WriteLine("Informe a data de lançamento do livro: ");
+    lancamento = DateOnly.Parse(Console.ReadLine());
 
-            case 2:
-                livro.ImprimirLivros();
-                break;
+    Console.WriteLine("Informe a editora do livro: ");
+    editora = Console.ReadLine();
 
-            case 3:
-                livro.ImprimirPosicao();
-                break;
+    Console.WriteLine("Informe a edição do livro: ");
+    edicao = int.Parse(Console.ReadLine());
 
-            default:
-                TrueFalse = true;
-                Console.WriteLine("\n|Opcão inválida!");
-                break;
+    Console.WriteLine("Informe o ISBN do livro: ");
+    isbn = Console.ReadLine();
 
-        }
-    }
-    Menu();
-    Console.ReadLine();
-    Console.Clear();
+    Console.WriteLine("Informe a quantidade de páginas do livro: ");
+    paginas = int.Parse(Console.ReadLine());
+
+    return new(titulo, autor, lancamento, editora, edicao, isbn, paginas);
+    //RETORNO DOS VALORES DA VARIAVEIS PARA O CONSTRUTOR DA CLASSE LIVRO
+
 }
+
+
+//LOOPING PARA CADASTRAR LIVROS USANDO DO WHILE 
+do
+{
+    Livro livro = cadastrarLivro();
+    estante[indice] = livro;
+    indice++;
+    Console.WriteLine("Deseja cadastrar um novo livro?");
+    op = int.Parse(Console.ReadLine());
+
+} while (op != 0 && indice < 10);
+
+
+//LOOPING PARA IMPRIMIR LIVROS USANDO FOR
+for(int i = 0; i <= i; i++)
+{
+    estante[i].imprimirLivro();
+}
+
+
+Console.ReadLine();
